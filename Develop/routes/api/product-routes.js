793a39da90610dc76313e140a7d75b9eba502eb2
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-02-05 23:51:25 -0500

    built out the product routes js file and created my readme with a screen recording for the demo

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{
+      model: Category, Tag
+    }]
+  }).then((productsData)=>{
+    res.status(200).json(productsData)
+  }).catch((err)=>{
+    res.status(500).json(err)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id,{
+    include: [{
+      model: Category, Tag
+    }]
+  }).then(productData=> {
+    res.status(200).json(productData)
+  }).catch((err)=>{
+    res.status(500).json(err)
+  })
 });
 
 // create new product
@@ -94,6 +112,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.deleteOne({_id: req.params.id})
+  .then(deleteOne=>{
+    res.status(200).json(deleteOne)
+  }).catch((err)=>{
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
